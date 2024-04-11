2d4d6835962526d4406b2c0badb742c10e646d7c
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2024-04-11 02:01:26 -0700

    add login page ux

---
+++ src/app/login/page.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image"
+import Link from "next/link"
+import { FaFacebook, FaGoogle } from "react-icons/fa"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+
+export default function LoginPage() {
+  return (
+    <div className="w-full lg:grid lg:grid-cols-2 h-screen">
+      <div className="flex items-center justify-center py-12 h-full">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Input
+                id="email"
+                type="email"
+                placeholder="john@example.com"
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Continue
+            </Button>
+            <Separator />
+            <Button variant="outline" className="w-full">
+              <FaGoogle className="mr-2" /> Continue with Google
+            </Button>
+            <Button variant="outline" className="w-full">
+              <FaFacebook className="mr-2" /> Continue with Facebook
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/soccer-game.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover brightness-[0.5] grayscale-[50%]"
+        />
+      </div>
+    </div>
+  )
+}
