c1ce8296547110194a2fd4064963a2cbc9dff034
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2024-04-17 22:58:17 -0700

    format dates in table

--- src/app/(protected)/players/columns.tsx
+++ src/app/(protected)/players/columns.tsx
@@ -11,9 +11,17 @@ export const columns: ColumnDef<Player>[] = [
   {
     accessorKey: "birthday",
     header: "Birthday",
+    cell(props) {
+      const value = props.getValue()
+      return value ? new Date(value as string).toLocaleDateString() : "-"
+    },
   },
   {
     accessorKey: "createdAt",
     header: "Member since",
+    cell(props) {
+      const value = props.getValue()
+      return value ? new Date(value as string).toLocaleDateString() : "-"
+    },
   },
 ]
